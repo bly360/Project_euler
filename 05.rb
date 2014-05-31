32373132686eb3ebbbb7be58257c95b692905709
Elaine Trombley <elaine.trombley3@gmail.com>
2014-05-31 13:04:44 -0400

    added problems

--- 05.rb
+++ 05.rb
@@ -1,5 +1,12 @@
 # 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
-def method_name
+def evenly_div
+num = 20
 
+  until (11..20).all?{ |i| num % i == 0 }
+    num +=20
+  end
+
+  num
 end
+p evenly_div
